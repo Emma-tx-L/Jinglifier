003e71a82a7072d50961130040c6a4971e687526
Emma Liu <gigitxl@yahoo.ca>
2019-04-30 21:04:24 -0700

    added basic UI

--- src/main/ui/Jinglifier.java
+++ src/main/ui/Jinglifier.java
@@ -1,46 +1,66 @@
 package ui;
 
-import javax.swing.*;
-import java.awt.*;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import model.NotePlayer;
 
-public class Jinglifier extends JFrame {
-    public static final int WIDTH = 800;
-    public static final int HEIGHT = 600;
-
-    public Jinglifier() {
-        super("Jinglifier");
-        initializeGraphics();
-    }
-
-    private void initializeGraphics() {
-        setLayout(new GridBagLayout());
-        setMinimumSize(new Dimension(WIDTH, HEIGHT));
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        createUIComponents();
-        setVisible(true);
-    }
-
-    private void createUIComponents() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridBagLayout());
-
-        Container cont = getContentPane();
-        GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.gridx = 0;
-        c.gridy = 0;
-        c.gridwidth = 50;
-        InputBox box1 = new InputBox(panel);
-        InputBox box2 = new InputBox(panel);
-        PlayButton button = new PlayButton();
-        //add(panel, c);
-        cont.add(box1, c);
-        cont.add(box2);
-        /*panel.add(button);*/
-    }
+public class Jinglifier extends Application {
+    private String input;
+    private NotePlayer player1;
 
     public static void main(String[] args) {
-        new Jinglifier();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        initializeNotePlayer();
+
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        grid.setVgap(5);
+        grid.setHgap(5);
+
+        final TextArea text = new TextArea ();
+        text.setText("Type something here!");
+        text.setPrefColumnCount(20);
+        text.setPrefRowCount(10);
+        text.setWrapText(true);
+        GridPane.setConstraints(text, 1,1);
+        grid.getChildren().add(text);
+
+        Button btn = new Button();
+        btn.setText("Jinglify!");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                input = text.getText();
+                player1.playString(input);
+            }
+        });
+        GridPane.setConstraints(btn, 1,2);
+        grid.getChildren().add(btn);
+
+
+/*        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        root.getChildren().add(text);
+        primaryStage.setScene(new Scene(root, 300, 250));*/
+
+        primaryStage.setScene(new Scene(grid, 300, 250));
+        primaryStage.show();
+    }
+
+    private void initializeNotePlayer() {
+        player1 = new NotePlayer();
     }
 }
